6eedd7a4e56401c349075ea7473970670a82348c
jenisa maretasya <maretasyaj@gmail.com>
2024-05-03 22:33:08 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: [" Now I'm Mentee in Infinite Learning Indonesia"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 100,
+    loop: true,
+})
